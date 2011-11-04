6172e4c8c73fa02a9d4018e3625fdfda4f5f86b1
charlesmarshall <cm56marshall@googlemail.com>
2011-11-04 11:03:34 +0000

    change the action name so its matches what the form will be

--- lib/controller/WildfireCompetitionController.php
+++ lib/controller/WildfireCompetitionController.php
@@ -3,7 +3,7 @@ class WildfireCompetitionController extends WaxController{
   
   public $entry_model_class = "WildfireCompetitionEntry";
   
-  public function _form(){
+  public function __competition_form(){
     $this->entry_model = new $this->entry_model_class
     $this->comp_form = new WaxForm($this->entry_model);
     if($saved = $this->comp_form->save()){
