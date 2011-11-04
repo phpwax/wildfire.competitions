64ce259500ccd79bd18adec629a441469c0c45b5
charlesmarshall <cm56marshall@googlemail.com>
2011-11-04 11:32:56 +0000

    add it in to the group

--- lib/model/WildfireCompetition.php
+++ lib/model/WildfireCompetition.php
@@ -5,10 +5,9 @@ class WildfireCompetition extends WildfireContent{
   public function setup(){
     parent::setup();
     $this->define("competition_item", "BooleanField", array('group'=>"competition"));
-    $this->define("competition_title", "CharField", array('label'=>"Title (internal use)", 'group'=>"competition"));
     $this->define("competition_content", "TextField", array('label'=>"Introduction", 'group'=>"competition"));
-    $this->define("competition_date_start", "DateTimeField", array('default'=>date("Y-m-d h:i:s"), 'output_format'=>"j F Y",'input_format'=> 'j F Y H:i', 'info_preview'=>1));
-		$this->define("competition_date_end", "DateTimeField", array('default'=>date("Y-m-d h:i:s",mktime(0,0,0, date("m"), date("j"), date("y")-10 )), 'output_format'=>"j F Y", 'input_format'=> 'j F Y H:i','info_preview'=>1));
+    $this->define("competition_date_start", "DateTimeField", array('default'=>date("Y-m-d h:i:s"), 'output_format'=>"j F Y",'input_format'=> 'j F Y H:i', 'group'=>'competition'));
+		$this->define("competition_date_end", "DateTimeField", array('default'=>date("Y-m-d h:i:s",mktime(0,0,0, date("m"), date("j"), date("y")-10 )), 'output_format'=>"j F Y", 'input_format'=> 'j F Y H:i','group'=>'competition'));
 
     if(!$comp_fields = Config::get("competition/fields")){
       $comp_fields = array(
