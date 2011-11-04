a8c2a4b2d246264f01499df810de71e45fb9047e
charlesmarshall <cm56marshall@googlemail.com>
2011-11-04 11:40:00 +0000

    cms admin module

---
+++ lib/controller/CMSAdminCompetitionentryController.php
@@ -0,0 +1,12 @@
+<?php
+
+class CMSAdminCompetitionentryController extends AdminComponent {
+
+  public $module_name = "competitionentry";
+  public $model_class = 'WildfireCompetitionEntry';
+	public $display_name = "Competition Entries";
+  public $dashboard = false;
+
+}
+
+?>
