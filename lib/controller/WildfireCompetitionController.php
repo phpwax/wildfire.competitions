3e5e17b57262b8fb9efe5da7395cdfa40efcc602
charlesmarshall <cm56marshall@googlemail.com>
2011-11-04 11:32:43 +0000

    add missing ;

--- lib/controller/WildfireCompetitionController.php
+++ lib/controller/WildfireCompetitionController.php
@@ -4,7 +4,7 @@ class WildfireCompetitionController extends WaxController{
   public $entry_model_class = "WildfireCompetitionEntry";
   
   public function __competition_form(){
-    $this->entry_model = new $this->entry_model_class
+    $this->entry_model = new $this->entry_model_class;
     $this->comp_form = new WaxForm($this->entry_model);
     if($saved = $this->comp_form->save()){
       $this->redirect_to("/thanks/competition/");
