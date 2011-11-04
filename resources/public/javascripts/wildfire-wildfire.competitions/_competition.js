597af2949da7b7b72cab0c500a396f163dfec391
charlesmarshall <cm56marshall@googlemail.com>
2011-11-04 10:39:32 +0000

    hide all the form fields when value is empty

--- resources/public/javascripts/wildfire-wildfire.competitions/_competition.js
+++ resources/public/javascripts/wildfire-wildfire.competitions/_competition.js
@@ -2,7 +2,11 @@ jQuery(document).ready(function(){
   jQuery("div.comp_spliter").hide();
   jQuery("#wildfire_content_competition_type").change(function(){
     var val = jQuery(this).val();
-    jQuery("div.comp_spliter:not(."+val+")").slideUp("fast");
-    jQuery("div.comp_spliter."+val).slideDown("fast");
+    if(val){
+      jQuery("div.comp_spliter:not(."+val+")").slideUp("fast");
+      jQuery("div.comp_spliter."+val).slideDown("fast");
+    }else{
+      jQuery("div.comp_spliter").slideUp("fast");
+    }
   });
 });
