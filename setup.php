73fd49ce605493ca3f2d588f49bd957490b119cc
charlesmarshall <cm56marshall@googlemail.com>
2011-11-07 10:51:24 +0000

    competition makes use of wildfire.dynamic_content plugin now, hooks in to the setup event to add in the custom fields it needs.

--- setup.php
+++ setup.php
@@ -1,3 +1,34 @@
 <?
 CMSApplication::register_module("competitionentry", array("display_name"=>"Competition Entries", "link"=>"/admin/competitionentry/"));
+
+
+//add in the custom model setup for the competition
+WaxEvent::add("wildfire_dynamic_content.setup", function(){
+  $obj = WaxEvent::data();
+  $obj->define("competition_item", "BooleanField", array('group'=>"competition"));
+  $obj->define("competition_content", "TextField", array('label'=>"Introduction", 'group'=>"competition"));
+  $obj->define("competition_date_start", "DateTimeField", array('default'=>date("Y-m-d h:i:s"), 'output_format'=>"j F Y",'input_format'=> 'j F Y H:i', 'group'=>'competition'));
+	$obj->define("competition_date_end", "DateTimeField", array('default'=>date("Y-m-d h:i:s",mktime(0,0,0, date("m"), date("j"), date("y")-10 )), 'output_format'=>"j F Y", 'input_format'=> 'j F Y H:i','group'=>'competition'));
+	
+  if(!$comp_fields = Config::get("competition/fields")){
+    $comp_fields = array(
+                    'multiple_choice'=>array(
+                        array('name'=>'multiple_choice_question', 'type'=>'CharField', 'options'=>array('group'=>'competition', 'sub'=>'multiple_choice')),
+                        array('name'=>'multiple_choice_option_a', 'type'=>'CharField', 'options'=>array('group'=>'competition', 'sub'=>'multiple_choice')),
+                        array('name'=>'multiple_choice_option_b', 'type'=>'CharField', 'options'=>array('group'=>'competition', 'sub'=>'multiple_choice')),
+                        array('name'=>'multiple_choice_option_c', 'type'=>'CharField', 'options'=>array('group'=>'competition', 'sub'=>'multiple_choice'))
+                      ),
+                      'free_text'=>array(
+                        array('name'=>'free_text_question', 'type'=>'CharField', 'options'=>array('group'=>'competition', 'sub'=>'free_text')),
+                      )
+                    );
+  }
+  $types = array(''=>'-- select --');
+  foreach(array_keys($comp_fields) as $name) $types[$name] = ucwords(str_replace("_", " ", $name));
+  $obj->define("competition_type", "CharField", array('widget'=>'SelectInput', 'choices'=>$types, 'group'=>"competition"));
+  
+  foreach($comp_fields as $type=>$config) foreach($config as $field) $obj->define($field['name'], $field['type'], $field['options']);
+  
+});
+
 ?>
